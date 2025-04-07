597fe57eeaf19c2ed05c1faae9f85a6d596f800f
daishuboluo <daishuboluo@Gmail.com>
2025-04-08 04:48:14 +0800

    4/8

--- Anime_Template_Project/build/Desktop_Qt_6_8_2_MSVC2022_64bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Anime_Template_Project_FILE C:/Users/Ld/Desktop/Cjiajia/qt/Anime_Template_Project/Anime_Template_Project/build/Desktop_Qt_6_8_2_MSVC2022_64bit-Release/Anime_Template_Project.exe)
-set(__QT_DEPLOY_TARGET_Anime_Template_Project_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Anime_Template_Project_RUNTIME_DLLS C:/Qt/6.8.2/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.8.2/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/msvc2022_64/bin/Qt6Core.dll)

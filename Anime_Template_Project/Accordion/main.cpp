2ee8757fed60d1b08f282a00af982dd09cc90129
zouri <2823730019@qq.com>
2025-07-05 23:23:02 +0800

    first commit

--- Anime_Template_Project/Accordion/main.cpp
+++ Anime_Template_Project/Accordion/main.cpp
@@ -1,9 +1,10 @@
 #include <QApplication>
 #include <QMainWindow>
-#include "AccordionWidget.h"
-#include "AccordionCard.h"
 
-int main(int argc, char *argv[])
+#include "AccordionCard.h"
+#include "AccordionWidget.h"
+
+int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
 
@@ -11,13 +12,13 @@ int main(int argc, char *argv[])
     window.setWindowTitle("手风琴");
     window.setMinimumSize(800, 300);
 
-    AccordionWidget *accordion = new AccordionWidget(&window);
+    AccordionWidget* accordion = new AccordionWidget(&window);
 
-    accordion->addCard(new AccordionCard("卡片 1", "://img/FvCpS5naYAA06ej.jpg"));
-    accordion->addCard(new AccordionCard("卡片 2", "://img/GS6v-JebIAIMmNY.jpg"));
-    accordion->addCard(new AccordionCard("卡片 3", "://img/GniQBbrbUAAq-HX.jpg"));
-    accordion->addCard(new AccordionCard("卡片 4", "://img/GGE5u19acAAa7OA.jpg"));
-    accordion->addCard(new AccordionCard("卡片 5", "://img/GGIv8piaUAA_myh.jpg"));
+    accordion->addCard(new AccordionCard("卡片 1", ":/img/FvCpS5naYAA06ej.jpg"));
+    accordion->addCard(new AccordionCard("卡片 2", ":/img/GS6v-JebIAIMmNY.jpg"));
+    accordion->addCard(new AccordionCard("卡片 3", ":/img/GniQBbrbUAAq-HX.jpg"));
+    accordion->addCard(new AccordionCard("卡片 4", ":/img/GGE5u19acAAa7OA.jpg"));
+    accordion->addCard(new AccordionCard("卡片 5", ":/img/GGIv8piaUAA_myh.jpg"));
 
     window.setCentralWidget(accordion);
     window.show();
